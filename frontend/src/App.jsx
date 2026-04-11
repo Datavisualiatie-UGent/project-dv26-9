99c66d37c9989fac53442a74fb5913852b7e4a3d
Nyah Van Wayenberge <nyah.vanwayenberge@ugent.be>
2026-04-11 20:09:57 +0200

    feat: added router to navigate all pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,22 @@
-import { useState } from 'react'
+import { Routes, Route, Link } from 'react-router-dom'
 import './App.css'
+
+import ConflictWrapper from './plots/ConflictWrapper';
 import InteractiveLinePlot from './plots/InteractiveLinePlot';
 
 function App() {
   return <div>
     <h1>Datavisualisatie Groep 9</h1>
-    <InteractiveLinePlot/>
+
+      <nav>
+        <Link to="/conflicts">Conflict</Link> |{" "}
+        <Link to="/lineplot">Interactive Lineplot</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/conflicts" element={<ConflictWrapper />} />
+        <Route path="/lineplot" element={<InteractiveLinePlot />} />
+      </Routes>
   </div>;
 }
 
