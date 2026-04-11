1fb4f19e5dc5d2e9a41af37cfce0a53b2931065a
Nyah Van Wayenberge <nyah.vanwayenberge@ugent.be>
2026-04-11 20:07:48 +0200

    feat: fixed formatting of lineplot (year on axis, expenditure in label)

--- frontend/src/plots/LinePlot.jsx
+++ frontend/src/plots/LinePlot.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useRef, useState } from 'react'
 import '../style/LinePlot.css'
 import * as Plot from "@observablehq/plot";
-import { csvParse, autoType } from "d3-dsv";
 
 
 function LinePlot({ data }) {
     const ref = useRef();
+    const fmt = new Intl.NumberFormat("en-US");
 
     // Render line plot when data changes
     useEffect(() => {
@@ -15,47 +15,59 @@ function LinePlot({ data }) {
         const linePlot = Plot.plot({
             y: {axis: "right", grid: true, nice: true},
             marks: [
-                Plot.lineY(data, {x: "Year", y: "Military_expenditure"}),
-                Plot.dot(data, {x: "Year", y: "Military_expenditure", r: 2}),
+                Plot.lineY(data, {
+                    x: "Year", 
+                    y: "Military_expenditure",
+                    stroke: "Entity"
+                }),
+                
+                Plot.dot(data, {
+                    x: "Year", 
+                    y: "Military_expenditure", 
+                    r: 2,
+                    stroke: "Entity",
+                    fill: "Entity"
+                }),
                 
                 Plot.crosshair(data, {
-                x: "Year", 
-                y: "Military_expenditure", 
-                color: "red", 
-                opacity: 0.5,
-                textFillOpacity: 0,
-                textStrokeOpacity: 0
+                    x: "Year", 
+                    y: "Military_expenditure", 
+                    color: "red", 
+                    opacity: 0.5,
+                    textFillOpacity: 0,
+                    textStrokeOpacity: 0
                 }),
                 
                 Plot.dot(data, Plot.pointer({
-                x: "Year", 
-                y: "Military_expenditure", 
-                r: 5,
-                stroke: "red", 
-                opacity: 0.5
+                    x: "Year", 
+                    y: "Military_expenditure", 
+                    r: 5,
+                    stroke: "red", 
+                    opacity: 0.5
                 })),
                 
-                Plot.text(data, Plot.pointerX({
-                px: "Year", 
-                py: "Military_expenditure", 
-                dy: -17, 
-                frameAnchor: "top-right", 
-                fontVariant: "tabular-nums", 
-                text: (d) => [`Year: ${d.Year}`, `Military expenditure: ${d["Military_expenditure"]} USD`].join("   "),
-                fontSize: 13
+                Plot.text(data, Plot.pointer({
+                    px: "Year", 
+                    py: "Military_expenditure", 
+                    dy: -17, 
+                    frameAnchor: "top-right", 
+                    fontVariant: "tabular-nums", 
+                    text: (d) => [`Year: ${d.Year}`, `Military expenditure: ${fmt.format(d.Military_expenditure)} USD`].join("   "),
+                    fontSize: 13
                 })),
 
             ],
             x: {
                 label: "Year",
-                grid: true
+                grid: true,
+                tickFormat: d => `${d}`
             },
             y: {
-                label: "Military expenditure (×10⁹)",
+                label: "Military expenditure in USD (×10⁹)",
                 grid: true,
                 tickFormat: d => d / (10**9)  // divide by 1000 for labels
             }
-        });
+            });
 
         ref.current.innerHTML = ""; 
 
